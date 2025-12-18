3134fe9a0717f0277983feb4419d7d4e0afc814e
chandraobula <chandraobulareddy.theegala@gmail.com>
2025-12-18 10:39:10 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { motion } from 'framer-motion'
-import Navigation from './components/Navigation'
-import Hero from './components/Hero'
-import About from './components/About'
-import WebShowcases from './components/WebShowcases'
-import Experience from './components/Experience'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-
+import { motion } from "framer-motion";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import WebShowcases from "./components/WebShowcases";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+//import Footer from './components/Footer'
 function App() {
   return (
     <div className="min-h-screen bg-dark-bg overflow-x-hidden">
@@ -22,7 +22,7 @@ function App() {
         <Contact />
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
