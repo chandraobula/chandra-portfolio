a20f0580e5171df76f7c380d2b61e0bbbdaa124e
chandraobula <chandraobulareddy.theegala@gmail.com>
2025-12-18 11:01:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-//import Footer from './components/Footer'
+//import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-dark-bg overflow-x-hidden">
